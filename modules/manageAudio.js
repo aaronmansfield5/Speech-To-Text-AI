2a1587b9d2206e68a36350a67cd2b8850c182c6d
Aaron Mansfield <37600872+aaronmansfield5@users.noreply.github.com>
2023-03-15 22:17:32 +0000

    Create manageAudio.js

---
+++ modules/manageAudio.js
@@ -0,0 +1,70 @@
+const shell = require('shelljs');
+const textToSpeech = require('@google-cloud/text-to-speech');
+const textToSpeechClient = new textToSpeech.TextToSpeechClient({
+    projectId: '',
+    keyFilename: ''
+});
+const fs = require('fs');
+const stream = require('stream');
+
+function sound() {
+    this.play = function (mp3Location) {
+        this.stop();
+        shell.exec(mp3Location, {
+            silent: true
+        });
+        return true;
+    };
+
+    this.stop = function () {
+        shell.exec('TASKKILL /IM VLC.EXE', {
+            silent: true
+        });
+        return true;
+    };
+
+    this.playResponse = function () {
+        this.stop()
+        shell.exec('"C:\\Program Files\\VideoLAN\\VLC\\vlc.exe" --play-and-exit --qt-start-minimized "C:\\Users\\username\\Desktop\\vr-ai\\response\\output.wav"', {
+            silent: true
+        })
+    };
+
+    this.create = function (response) {
+        const request = {
+            input: {
+                text: response
+            },
+            voice: {
+                languageCode: 'en-GB'
+            },
+            audioConfig: {
+                audioEncoding: 'LINEAR16',
+                speakingRate: 1,
+                pitch: 0.5
+            },
+        };
+
+        textToSpeechClient.synthesizeSpeech(request,
+            (err, response) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+
+                const audioStream = new stream.PassThrough();
+                audioStream.end(response.audioContent);
+
+                const fileStream = fs.createWriteStream('./response/output.wav');
+                audioStream.pipe(fileStream);
+
+                fileStream.on('finish', () => {
+                    this.playResponse();
+                });
+            });
+    }
+}
+
+module.exports = {
+    sound
+}
